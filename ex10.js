0f48b4decdcbc38c6bb49a66b839d283fd1944ab
Bisma786 <159830145+Bisma786@users.noreply.github.com>
2024-02-26 21:21:47 +0500

    Initial commit

---
+++ ex10.js
@@ -0,0 +1,14 @@
+// Q:10 Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//I have written this program on 16/2/2024 and it prints a quote of my favourite celebrity.
+var famous_person = "Andrew Tate";
+var message = famous_person + "once said, \" Do the impossible and you will never doubt yourself again\"";
+console.log(message);
+//I have written this code on 17/2/2024 and shows different operations, priniting the same result from each.
+var additionResult = 4 + 4;
+console.log("Addition Result:", additionResult);
+var subtractionResult = 10 - 2;
+console.log("Subtraction result:", subtractionResult);
+var multiplicationResult = 2 * 4;
+console.log("Multiplication result:", multiplicationResult);
+var divisionResult = 16 / 2;
+console.log("Division result:", divisionResult);
---
+++ ex10.ts
@@ -0,0 +1,23 @@
+// Q:10 Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+
+
+//I have written this program on 16/2/2024 and it prints a quote of my favourite celebrity.
+let famous_person:string="Andrew Tate"
+let message:string= famous_person+`once said, " Do the impossible and you will never doubt yourself again"`
+console.log(message)
+
+
+
+//I have written this code on 17/2/2024 and shows different operations, priniting the same result from each.
+let additionResult:number= 4+4
+console.log("Addition Result:"additionResult)
+
+let subtractionResult:number=10-2
+console.log("Subtraction result:"subtractionResult)
+
+let multiplicationResult:number=2*4
+console.log("Multiplication result:"multiplicationResult)
+
+let divisionResult:number=16/2
+console.log("Division result:"divisionResult)
